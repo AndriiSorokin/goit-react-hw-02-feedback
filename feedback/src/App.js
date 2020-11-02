717927e4c5ad6c1752748de10fe13ab3f93c698b
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2020-11-02 18:32:09 +0200

    сделал второй таск

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import Feedback from './Feedback/Feedback';
 
+class App extends Component {
 
-const App = () => {
-  return (
-    <>
-      < Feedback />
-      </>
-  );
-};
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  }
+
+    getData = (e) => {
+    const name = e.target.name;
+    this.setState(prevState => ({
+      [name]: prevState[name]+1
+    }))
+  }
+
+  getTotal = () => {
+    return this.state.good + this.state.bad + this.state.neutral
+  }
+
+  getPositiveFeedBack = () => {
+    return this.state.good / this.getTotal()*100
+  }
+
+
+
+  render() {
+     const { good, bad, neutral} = this.state;
+    return (
+      <Feedback
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        getData={this.getData}
+        getTotal={this.getTotal}
+        getPositiveFeedBack={this.getPositiveFeedBack} />
+    );
+  }
+}
 
 export default App;
+
+
--- feedback/src/Feedback/Feedback.js
+++ feedback/src/Feedback/Feedback.js
@@ -1,58 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Feedback.css'
 
-class Feedback extends Component {
-
-  state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-  }
-
-  goodE = () => {
-    console.log(this);
-    this.setState((prevState) => ({
-      good: prevState.good+1
-    }))
-  }
-
-   neutralE = () => {
-    console.log(this);
-    this.setState((prevState) => ({
-      neutral: prevState.neutral+1
-    }))
-   }
-
-   badE = () => {
-    console.log(this);
-    this.setState((prevState) => ({
-      bad: prevState.bad+1
-    }))
-   }
-
-  countTotalFeedback = () => {
-   
-  }
-
-
-
-  render() {
+const Feedback = ({good, neutral, bad, getData, getTotal,getPositiveFeedBack}) => {
     return (
       <div className="container">
         <h1>Please Leave Feedback</h1>
-        <button onClick={this.goodE} className="btn btn1">Good</button>
-        <button onClick={this.neutralE} className="btn btn2">Neutral</button>
-        <button onClick={this.badE} className="btn btn3">Bad</button>
+        <button onClick={getData} name="good" className="btn btn1">Good</button>
+        <button onClick={getData} name="neutral" className="btn btn2">Neutral</button>
+        <button onClick={getData} name="bad" className="btn btn3">Bad</button>
 
         <h2>Statistic:</h2>
         <ul className="list">
-          <li className="item">Good:{this.state.good}</li>
-          <li className="item">Neutral:{this.state.neutral}</li>
-          <li className="item">Bad:{this.state.bad}</li>
+          <li className="item">Good:{good}</li>
+          <li className="item">Neutral:{neutral}</li>
+          <li className="item">Bad:{bad}</li>
+          <li className="item">Total:{getTotal()}</li>
         </ul>
-        <h2>Positive Feedback:</h2>
+        <h2>Positive Feedback:{getTotal() ? getPositiveFeedBack().toFixed(0): 0}%</h2>
       </div>
     )
   }
-}
 export default Feedback;
